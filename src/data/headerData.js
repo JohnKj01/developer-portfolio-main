7dd8c437024132aeb4927f917bd71b9cdd392428
John <johnkaranjairungu@gmail.com>
2025-11-07 10:54:03 +0300

    add profile image

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'John Karanja',
     title: "Web Developer",
     desciption:"Life may be a meandering path but don't worry about it, rather enjoy it. It is about the journey, not the destination so explore all that comes your way, learn and grow from it, for this journey only happens once.",
-    image: 'https://media.licdn.com/dms/image/v2/D4D03AQE1dABmgd8EtA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1697567302446?e=1755734400&v=beta&t=74kWQfZt_FY_Y9VGZ4YpTC2p3AWz_AiLC4ez_SEvyXI',
+    image: './assets/png/profilepic.png',
     resumePdf: resume
 }
