29f62a116f5a328c3ff9d6b81f0616ad62c30908
John <johnkaranjairungu@gmail.com>
2025-11-07 10:27:11 +0300

    add image

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,7 @@
+
 export const aboutData = {
     title: "Who I am",
     description1: "Hello! My name is John Karanja. I'm a web developer based in Kenya.",
     description2: "I am an IT Support Associate at Ricolte Limited, Nairobi. I have previously worked as a developer with various companies including SmartLegal Africa and Tuzuruwapi. I have also assisted in data collection and management for organizations such as Kalro, CIP and Nickel Marketing ltd ",
-    image: 2
+    image: src="https://media.licdn.com/dms/image/v2/D4D03AQHK66R-A_uIzA/profile-displayphoto-crop_800_800/B4DZhfsozvGgAI-/0/1753952195628?e=1764201600&v=beta&t=x9K8jUi-YUX0K9cUphqdzu9goF0UwAjClYGtRt4Jv68",
 }
